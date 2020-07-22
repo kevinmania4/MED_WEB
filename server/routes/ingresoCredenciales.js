72ad46cc58ec182b6175e288347236c62857e6a8
wjuma <52583234+wjuma@users.noreply.github.com>
2020-07-22 16:56:00 -0500

    REGISTRO

--- server/routes/ingresoCredenciales.js
+++ server/routes/ingresoCredenciales.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const _ = require("underscore");
 const Usuario = require("../models/usuario");
-
+const { verificaToken, ver_doc } = require("../middlewares/autenticacion");
 const app = express();
-
+/*
 app.get("/logeo", (req, res) => {
     res.render("GUIiniciarsesion");
 });
@@ -39,5 +40,40 @@ app.post("/logeo", (req, res) => {
         res.send(vari);
     });
 });
+*/
+app.get("/logeo", (req, res) => {
+    res.render("GUIiniciarsesion");
+});
+app.post("/logeo", ver_doc, (req, res) => {
+    let body = req.body;
+    Usuario.findOne({ correo: body.correo }, (err, usuarioDB) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                err,
+            });
+        }
+        if (!usuarioDB) {
+            return res.status(400).json({
+                ok: false,
+                err: { message: "Usuario.. o contraseña incorrectos" },
+            });
+        }
 
+        if (!bcrypt.compareSync(body.contrasenia, usuarioDB.contrasenia)) {
+            return res.status(400).json({
+                ok: false,
+                err: { message: "Usuario o contraseña.. incorrectos" },
+            });
+        }
+        //genera token
+        let token = jwt.sign({
+                usuario: usuarioDB,
+            },
+            process.env.SEED, { expiresIn: process.env.CADUCIDAD_TOKEN }
+        );
+        // res.json({ ok: true, usuario: usuarioDB, token });
+        res.send("mensaje login post");
+    });
+});
 module.exports = app;
