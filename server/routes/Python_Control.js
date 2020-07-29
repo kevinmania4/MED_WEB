c484be77e20f89aa8225ae7c2e76acb2e07ae0ad
wjuma <52583234+wjuma@users.noreply.github.com>
2020-07-29 00:24:31 -0500

    diseño

--- server/routes/Python_Control.js
+++ server/routes/Python_Control.js
@@ -1,39 +1,37 @@
 const express = require("express");
 const app = express();
 
-app.get('/python', (req, res) => {
-    res.render('GUI_AM');
+app.get("/python", (req, res) => {
+    res.render("GUI_AM");
 });
 
 let cargaData = () => {
     return new Promise(function(sucess, nosuccess) {
-        const { spawn } = require('child_process');
-        const pyprog = spawn("python3", ["example.py"]);
-        pyprog.stdout.on('data', function(data) {
+        const { spawn } = require("child_process");
+        const pyprog = spawn("python", ["example.py"]);
+        pyprog.stdout.on("data", function(data) {
             sucess(JSON.parse(data.toString()));
         });
-        pyprog.stderr.on('data', (data) => {
+        pyprog.stderr.on("data", (data) => {
             nosuccess(data);
         });
     });
-}
+};
 
 let respuesta = async() => {
     let resultado = await cargaData();
     //console.log("Resultado DTA-->", resultado);
     return resultado;
-}
+};
 
-
-app.get('/python2', (req, res) => {
+app.get("/python2", (req, res) => {
     console.log("Respuesta RESP---->", respuesta());
-    respuesta().
-    then((resultado) => {
-        //console.log('THEN___>--->>', resultado);
-        res.send({ res: resultado })
-    }).
-    catch();
-})
-
+    respuesta()
+        .then((resultado) => {
+            //console.log('THEN___>--->>', resultado);
+            res.send({ res: resultado });
+        })
+        .catch();
+});
 
 module.exports = app;
